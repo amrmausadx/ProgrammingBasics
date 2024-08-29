8bd0a0ba97e9379a0e8064b26ac2d873cde0b88b
Amr Mausad Sauber Muhammad <135426571+amrmausadx@users.noreply.github.com>
2024-08-29 17:21:50 +0300

    Add files via upload

---
+++ 03_CL/CL_Code/Basics_Abstraction03_Interfaces.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+
+namespace Programming_Basics_CL
+{
+    internal class Basics_Abstraction03_Interfaces
+    {
+        public static void Main_interface()
+        {
+            Employee e =new Employee();
+            Engineer eng = new Engineer();
+            //((ILiving)e).
+            test(e);
+            test((ILiving)eng);
+        }
+        public static void test (ILiving i)
+        {
+            //Employee zakaria;
+            Employee emp = i as Employee;
+            if (emp != null)
+            {
+                ((Employee)i).NID = "5";
+                Console.WriteLine("I am an Employee");
+            }
+            else
+            {
+                Console.WriteLine("I am not an Employee");
+            }
+        }
+    }
+    interface ILiving
+    {
+        void Born();
+        void Eat();
+        void Move();
+        void Sleep();
+        void Die();
+    }
+    class Person
+    {
+        public string NID = "", Name = "", Address = "";
+        public DateTime DateofBirth = DateTime.Now;
+    }
+    class Employee :Person,  ILiving
+    {
+        void ILiving.Born()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILiving.Die()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILiving.Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILiving.Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILiving.Sleep()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Engineer : ILiving
+    {
+        void ILiving.Born()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILiving.Die()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILiving.Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILiving.Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILiving.Sleep()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 04_DS/DS_Code/CirularQueue.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace ProgrammingBasics_DS
+{
+    //circular queue ده طبقناه فى السكشن
+    internal class CirularQueue
+    {
+        static int MAX;
+        int[] Queue;
+        int rear, front;
+        public CirularQueue()
+        {
+            rear = front = -1;
+            Console.Write("please enter size of queue : ");
+            MAX = int.Parse(Console.ReadLine());
+            Queue = new int[MAX];
+        }
+        bool IsEmpty()
+        {
+            return (front < 0);
+        }
+        internal void enqueu()
+        {
+            if (((rear + 1) % Queue.Length) == front)
+            {
+                Console.WriteLine("queue Overflow");
+            }
+            else
+            {
+                if (front == -1)
+                {
+                    front++;
+                }
+                rear = (rear + 1) % MAX;
+                Console.Write("Enter the Pushed item: ");
+                Queue[rear] = int.Parse(Console.ReadLine());
+            }
+        }
+        internal void deque()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue Underflow");
+            }
+            else
+            {
+                int value = Queue[front];
+                Console.WriteLine("The removed item is " + value);
+
+                if (front == rear)
+                {
+                    front = rear = -1;
+                }
+                else
+                {
+                    front = (front + 1) % MAX;
+                }
+            }
+        }
+        internal void Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue Underflow");
+                return;
+            }
+            else
+                Console.WriteLine("The topmost element of queue is : {0}", Queue[rear]);
+        }
+        internal void PrintQueue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue Underflow");
+                return;
+            }
+            else
+            {
+                if (front <= rear)
+                {
+                    Console.WriteLine("Items in the Queue are :");
+                    for (int i = front; i <= rear; i++)
+                    {
+                        Console.WriteLine(Queue[i]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Items in the Queue are :");
+                    for (int i = front; i <= MAX - 1; i++)
+                    {
+                        Console.WriteLine(Queue[i]);
+                    }
+                    for (int i = 0; i <= rear; i++)
+                    {
+                        Console.WriteLine(Queue[i]);
+                    }
+                }
+            }
+        }
+    }
+}
+/* 
+ * Educational Code
+ * Dr. Amr Mausad @ 2024
+ * http://amrmausad.com
+ */
